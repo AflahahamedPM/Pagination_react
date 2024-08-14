0ec352e7787ca28d6421c4c02206093cc8f50c2b
AflahAhamed00 <aflahahamed777@gmail.com>
2024-08-14 18:26:00 +0530

    Completed a simple pagination using react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Pagination from "./Pagination";
+
+function App() {
+  const number_of_pages = 10 
+  // let limit = 5
+  return (
+    <>
+    <h1 className="flex justify-center items-center font-semibold text-4xl p-3 mb-10">Simple pagination task</h1>
+      <Pagination numberOfPages={number_of_pages}/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pagination.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from "react";
+
+const Pagination = ({ numberOfPages }) => {
+  const [currentPage, setCurrentPage] = useState(1);
+
+  let limit = 5;
+
+  const handlePageClick = (e) => {
+    setCurrentPage(Number(e.target.innerHTML));
+  };
+
+  const handleIncrement = () => {
+    setCurrentPage((prevActivePage) => prevActivePage + 1);
+  };
+
+  const handleDecrement = () => {
+    setCurrentPage((prevActivePage) => prevActivePage - 1);
+  };
+
+  const showPages = (numberOfPages, limit, currentPage) => {
+    const pageDifference = numberOfPages - currentPage;
+    if (pageDifference < limit) {
+      return Array(limit)
+        .fill(0)
+        .map((_, index) => {
+          return numberOfPages - limit + 1 + index;
+        });
+    } else {
+      return Array(limit)
+        .fill(0)
+        .map((_, index) => {
+          return currentPage + index;
+        });
+    }
+  };
+
+  const pagesToBeShowed = showPages(numberOfPages, limit, currentPage);
+
+  return (
+    <>
+      <div className="flex justify-center">
+        <div className="w-1/4 flex justify-between">
+          {currentPage > 1 ? (
+            <button
+              className="font-semibold text-2xl"
+              onClick={handleDecrement}
+            >
+              {"<"}
+            </button>
+          ) : (
+            <button className="text-2xl cursor-default text-gray-300">
+              {"<"}
+            </button>
+          )}
+
+          <div className="flex justify-between" onClick={handlePageClick}>
+            {pagesToBeShowed.map((pageValue, i) => (
+              <span
+                id={i + 1}
+                className={`p-2 ${
+                  currentPage === pageValue && `bg-gray-300`
+                } rounded-full w-10 h-10 flex items-center justify-center  mr-3 cursor-pointer hover:bg-gray-200`}
+                key={i + 1}
+              >
+                {pageValue}
+              </span>
+            ))}
+            
+          </div>
+          {currentPage != numberOfPages ? (
+            <button
+              className="font-semibold text-2xl"
+              onClick={handleIncrement}
+            >
+              {">"}
+            </button>
+          ) : (
+            <button className="text-2xl cursor-default text-gray-300">
+              {">"}
+            </button>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Pagination;
